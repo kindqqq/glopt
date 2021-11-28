707edb1183c2fe58a036927ea8554457e78317ec
Alexey Kapitonov <markmorozov@google.com>
2021-11-28 19:30:37 +0300

    add hamburger and change px on rem

---
+++ src/blocks/components/hamburger/hamburger.js
@@ -0,0 +1,30 @@
+import $ from "jquery";
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".menu"),
+    menuItem = document.querySelectorAll(".menu__item"),
+    hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger__active");
+    menu.classList.toggle("menu__active");
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger__active");
+      menu.classList.toggle("menu__active");
+    });
+  });
+
+  
+  $("ul.menu").on(
+    "click",
+    "li:not(.menu__item__active)",
+    function () {
+      $(this)
+        .addClass("menu__item__active")
+        .siblings()
+        .removeClass("menu__item__active");
+    }
+  );
+});
