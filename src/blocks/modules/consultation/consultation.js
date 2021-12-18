06d582ad7c0b0b4c603d5d48c81842d9ac0dbf59
Alexey Kapitonov <markmorozov@google.com>
2021-12-18 11:28:38 +0300

    carusel added and fix phone mask

--- src/blocks/modules/consultation/consultation.js
+++ src/blocks/modules/consultation/consultation.js
@@ -1,41 +1,81 @@
 import "jquery";
 import "jquery-validation";
-import "jquery.maskedinput";
+import "owl.carousel";
 //Validate Form
 
-function validForm(form) {
-  $(form).validate({
-    rules: {
-      name: {
-        required: true,
-        minlength: 2,
+// Phone Mask
+$("document").ready(function () {
+  function validForm(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
       },
-      phone: "required",
-      email: {
-        required: true,
-        email: true,
+      messages: {
+        name: {
+          required: "Введите свое имя",
+          minlength: jQuery.validator.format(
+            "Имя должно быть не менне {0} знаков!"
+          ),
+        },
+        phone: "Пожалуйста, укажите ваш номер телефона",
+        email: {
+          required: "Пожалуйста, укажите вашу почту",
+          email:
+            "Ваш адрес электронной почты должен быть в формате name@domain.com",
+        },
       },
-    },
-    messages: {
-      name: {
-        required: "Введите свое имя",
-        minlength: jQuery.validator.format(
-          "Имя должно быть не менне {0} знаков!"
-        ),
+    });
+  }
+  validForm("#validForm");
+  // Phone mask
+  $("#phone").mask("+7(999)999-99-99");
+
+  $(".owl-carousel").owlCarousel({
+    // stagePadding: 330,
+    loop: true,
+    margin: 0,
+    items: 1,
+    lazyLoad: true,
+    responsive: {
+      0: {
+        stagePadding: 60,
       },
-      phone: "Пожалуйста, укажите ваш номер телефона",
-      email: {
-        required: "Пожалуйста, укажите вашу почту",
-        email:
-          "Ваш адрес электронной почты должен быть в формате name@domain.com",
+      600: {
+        stagePadding: 100,
+      },
+      1000: {
+        stagePadding: 200,
+      },
+      1200: {
+        stagePadding: 250,
+      },
+      1400: {
+        stagePadding: 250,
+      },
+      1600: {
+        stagePadding: 250,
+      },
+      1800: {
+        stagePadding: 250,
       },
     },
   });
-}
-validForm("#validForm");
 
-//Phone Mask
-// $('document').ready(function() {
-//     $("#name").mask("99/99/9999");
-// });
+  var owl = $(".owl-carousel");
+  $(".owl-next").click(function () {
+    owl.trigger("next.owl.carousel", [500]);
+  });
 
+  $(".owl-prev").click(function () {
+    owl.trigger("prev.owl.carousel", [500]);
+  });
+
+});
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/header/header";
 import "%modules%/footer/footer";
 import "%modules%/consultation/consultation";
+import "./jquery.maskedinput.js";
 import "../../blocks/components/hamburger/hamburger";
